bd184fa8f45969f36f050f839810ddd5b286e040
Michel <michelc@gmail.com>
2019-09-21 09:19:47 +0200

    Connexion à la base de donnée PostgreSQL.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const { Pool } = require("pg");
 
 // Création du serveur Express
 const app = express();
@@ -8,6 +9,16 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+
+// Connexion à la base de donnée PostgreSQL
+const pool = new Pool({
+  user: "mystere",
+  host: "xxxxx.elephantsql.com",
+  database: "mystere",
+  password: "untrucsecretquinarienafaireici",
+  port: 5432
+});
+console.log("Connexion réussie à la base de données");
 
 // Démarrage du serveur
 app.listen(3000, () => {
